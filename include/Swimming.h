c5bdfe1984540dc523954423a37684cddd271856
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-22 09:18:05 -0700

    typo bug fixed

--- include/Swimming.h
+++ include/Swimming.h
@@ -5,12 +5,23 @@
 #ifndef SWIMMING_H
 #define SWIMMING_H
 
+#include "WorkOut.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
 
-class Swimming {
+class Swimming : public Workout {
+public:
+    // constructor
+    Swimming(const string& name, const string& date, int duration);
 
+    // show workout info
+    void show() const override;
+
+    // calculate calories burned
+    double calculateCalories(double weight) const override;
 };
 
-
-
-#endif //SWIMMING_H
+#endif
