ce6b61cc6efda65ab4fea4613829f2469c796d14
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-22 09:32:14 -0700

    minor issues fixed

--- src/DisplayMenu.cpp
+++ src/DisplayMenu.cpp
@@ -14,12 +14,11 @@ void DisplayMenu::showMainMenu() const {
     cout << setw((width + 32) / 2) << "Please choose an option below:" << endl;
     cout << setfill('-') << setw(width) << "-" << endl;
 
-    cout << setw(5) << "" << "1) Calorie Checker" << endl;
+    cout << setw(5) << "" << "1) Log Workout + Calorie Calculator" << endl; // merged both functions for better personality
     cout << setw(5) << "" << "2) Workout Recommendation" << endl;
-    cout << setw(5) << "" << "3) Log Workout" << endl;
-    cout << setw(5) << "" << "4) Routine Assigner" << endl;
-    cout << setw(5) << "" << "5) Save and Read Workout Data" << endl;
-    cout << setw(5) << "" << "6) Exit" << endl;
+    cout << setw(5) << "" << "3) Routine Assigner" << endl;
+    cout << setw(5) << "" << "4) Save and Read Workout Data" << endl;
+    cout << setw(5) << "" << "5) Exit" << endl;
 
     cout << setfill('-') << setw(width) << "-" << endl << endl;
     cout << setw((width + 8) / 2) << ">> ";
