286f5b380daf95816bb67bc74c84e51edd0b5faa
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-22 09:19:28 -0700

    updated main to check new function

--- main.cpp
+++ main.cpp
@@ -1,20 +1,49 @@
 // main.cpp
-//  Fitness Tracker
-//  Created by Subigya Parajuli on 2/10/25.
-//
+// Fitness Tracker – Entry Point
+// Created by Subigya Parajuli
 
-
+#include "DisplayMenu.h"
+#include "workoutLogger.h"
+#include "workOut.h"
 #include <iostream>
-#include <iomanip>
 #include <limits>
-#include "include/workoutLogger.h"
-#include "include/DisplayMenu.h"
-#include "include/Running.h"
+#include <iomanip>
+
 using namespace std;
 
-
 int main() {
-    DisplayMenu menu;
-    menu.showMainMenu();
+    WorkoutLog log;  //updated logging system
+    int choice;
+
+    while (true) {
+
+        DisplayMenu menu;
+        menu.showMainMenu();
+        cin >> choice;
+
+        // User Handling
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << setw(10) << "" << "Invalid input. Please try again.\n\n";
+            continue;
+        }
+
+        switch (choice) {
+            case 1:
+                log.logWorkout(); //testing logging system
+                break;
+            case 6:
+                cout << setw(10) << "" << "Goodbye!\n";
+                return 0;
+            default:
+                cout << setw(10) << "" << "Invalid option. Please try again.\n";
+        }
+
+        cout << setw(10) << "" << "Press Enter to return to main menu...";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin.get();
+    }
+
     return 0;
 }
