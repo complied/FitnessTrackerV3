71cb9336dad7e724b6cbc1ff119211d0daa73310
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-22 02:44:15 -0700

    added running

--- include/Running.h
+++ include/Running.h
@@ -5,12 +5,23 @@
 #ifndef RUNNING_H
 #define RUNNING_H
 
+#include "Workout.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
 
+using namespace std;
 
-class Running {
+class Running : public Workout {
+public:
+    // created a constructor for running
+    Running(const string& name, const string& date, int duration);
 
+    // showing the workout based on the whats being updated
+    void show() const override;
+
+    // calculating the calories
+    double calculateCalories(double weight) const override;
 };
 
-
-
-#endif //RUNNING_H
+#endif
