d62ac57eb7743be1f291f56b929e30abd55ea2c5
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-22 08:56:38 -0700

    Updated main, way cleaner now

--- main.cpp
+++ main.cpp
@@ -8,46 +8,13 @@
 #include <iomanip>
 #include <limits>
 #include "include/workoutLogger.h"
-
+#include "include/DisplayMenu.h"
+#include "include/Running.h"
 using namespace std;
 
+
 int main() {
-    int choice;
-
-    while (true) {
-        // display menu
-        cout << setw(10) << "" << "====================" << endl;
-        cout << setw(10) << "" << "  FITNESS TRACKER" << endl;
-        cout << setw(10) << "" << "====================" << endl;
-        cout << setw(10) << "" << "1. Log Workout" << endl;
-        cout << setw(10) << "" << "Enter your choice: ";
-        cin >> choice;
-
-        // clear input if bad
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << setw(10) << "" << "Invalid input. Please try again.\n";
-            continue;
-        }
-
-        // switch case
-        switch (choice) {
-            case 1: {
-                // log workouts
-                WorkoutLog log;
-                log.logWorkout();
-                break;
-            }
-            default:
-                cout << setw(10) << "" << "Invalid choice. Try again.\n";
-        }
-
-        // pause
-        cout << setw(10) << "" << "Press Enter to return to menu...";
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cin.get();
-    }
-
+    DisplayMenu menu;
+    menu.showMainMenu();
     return 0;
 }
