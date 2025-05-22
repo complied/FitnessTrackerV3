f956d315d98283bd16a7923e87368b4210b9a4e4
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-22 02:43:04 -0700

    updated with psuedo code

--- src/Running.cpp
+++ src/Running.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Running.h"
+
+
+using namespace std;
+
+// constructor
+Running::Running(const string& name, const string& date, int duration)
+    : Workout(name, date, duration) {}
+
+// show workout info
+void Running::show() const {
+    cout << setw(10) << "" << "[Running] " << name << " | " << date << " | " << duration << " min" << endl;
+}
+
+// calculate calories burned
+double Running::calculateCalories(double weight) const {
+    double met = 9.8;
+    return 0.0175 * met * weight * duration;
+}
