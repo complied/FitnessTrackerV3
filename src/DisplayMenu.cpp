d78efdf6ac8c0fd662212139f6392e534ea401a6
Subigya Parajuli <subigyaparajulo009@gmail.com>
2025-05-22 08:56:07 -0700

    updated menu

--- src/DisplayMenu.cpp
+++ src/DisplayMenu.cpp
@@ -1,34 +1,26 @@
+#include "DisplayMenu.h"
 #include <iostream>
-using namespace std;
 #include <iomanip>
-#include <string>
 
-namespace displayMenu {
-    //created a displayMenu function that now shows us the basic menu output!
+using namespace std;
 
-    void displayMenu() {
-        const int width = 50;
+void DisplayMenu::showMainMenu() const {
+    const int width = 50;
 
-        cout << setfill('=') << setw(width) << "=" << endl;
-        cout << setfill(' ') << setw((width + 28) / 2) << "Welcome to the Fitness Tracker" << endl;
-        cout << setfill('=') << setw(width) << "=" << endl;
-        cout << endl;
+    cout << setfill('=') << setw(width) << "=" << endl;
+    cout << setfill(' ') << setw((width + 28) / 2) << "Welcome to the Fitness Tracker" << endl;
+    cout << setfill('=') << setw(width) << "=" << endl << endl;
 
-        cout << setw((width + 32) / 2) << "Please choose an option below:" << endl;
-        cout << setfill('-') << setw(width) << "-" << endl;
+    cout << setw((width + 32) / 2) << "Please choose an option below:" << endl;
+    cout << setfill('-') << setw(width) << "-" << endl;
 
-        cout << setfill(' ') << setw(5) << "" << "1) Calorie Checker" << endl;
-        cout << setw(5) << "" << "2) Workout Recommendation" << endl;
-        cout << setw(5) << "" << "3) Log Workout" << endl;
-        cout << setw(5) << "" << "4) Routine Assigner" << endl;
-        cout << setw(5) << "" << "5) Save and Read Workout Data" << endl;
+    cout << setw(5) << "" << "1) Calorie Checker" << endl;
+    cout << setw(5) << "" << "2) Workout Recommendation" << endl;
+    cout << setw(5) << "" << "3) Log Workout" << endl;
+    cout << setw(5) << "" << "4) Routine Assigner" << endl;
+    cout << setw(5) << "" << "5) Save and Read Workout Data" << endl;
+    cout << setw(5) << "" << "6) Exit" << endl;
 
-        cout << setfill('-') << setw(width) << "-" << endl;
-        cout << endl;
-        cout << setw((width + 8) / 2) << ">> ";
-    }
-
+    cout << setfill('-') << setw(width) << "-" << endl << endl;
+    cout << setw((width + 8) / 2) << ">> ";
 }
-
-
-// Used Basic Iomanip Libarries to create this!
